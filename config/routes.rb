6299714dae5f05595d331f8e99eb20eee2c0c9fc
Nicola Hauke <jasinai@jasinai.de>
2015-04-06 09:47:42 +0000

    Add post route to create recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'recipes/new' => 'recipes#new'
+  post 'recipes/' => 'recipes#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
