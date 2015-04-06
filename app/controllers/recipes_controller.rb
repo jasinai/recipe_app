54b5a4a770276b4705961d8262890b688d29124a
Nicola Hauke <jasinai@jasinai.de>
2015-04-06 10:04:59 +0000

    Add create action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,19 @@ class RecipesController < ApplicationController
   def new
     @recipe = Recipe.new
   end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+
+    if @recipe.save
+      redirect_to action: 'new', notice: 'Idea was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+    def recipe_params
+      params.require(:recipe).permit(:title, :ingredients, :preparations, :image)
+    end
 end
